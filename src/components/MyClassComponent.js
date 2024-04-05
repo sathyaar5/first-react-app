7270e633822db100bff3b96bfe5422f021fc0b39
Sathya Narayana Rangappagari <sathya.narayana@softsuave.org>
2024-04-05 18:23:39 +0530

    added class component and functional component example

---
+++ src/components/MyClassComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MyClassComponent extends Component {
+  render() {
+    return <div>Hello, I am a Class Component!</div>;
+  }
+}
+
+export default MyClassComponent;
